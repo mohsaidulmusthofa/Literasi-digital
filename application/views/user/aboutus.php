479752ffa70bd45ff63649e40cd98c9ebd806939
Shandy Maulana Yudantiar <shandy318@gmail.com>
2021-11-17 08:06:00 +0700

    about us

--- application/views/user/aboutus.php
+++ application/views/user/aboutus.php
@@ -5,7 +5,46 @@
           <p class="section-subtitle"></p>
         </div>
         <div class="row">
-          
+          <div class="col-lg-4">
+            <p style="font-weight: 600; text-align:center">
+              Creating an integrated export ecosystem between stakeholders
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <p style="font-weight: 600; text-align:center">
+              Empowering domestic production Improve quality, quantity, and continuity through collaboration between stakeholders
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <p style="font-weight: 600; text-align:center">
+              Increase employment and inclusive economic growth
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="features" class="section">
+      <div class="container">
+        <div class="section-header">          
+          <h2 class="section-subtitle" style="font-size: 1.2rem; background: none">Nusantara Coco Indo establishment with the three value of Sustainable Development Goals</h2>
+          <p class="section-subtitle"></p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4">
+            <p style="font-weight: 600; text-align:center">
+              Creating an integrated export ecosystem between stakeholders
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <p style="font-weight: 600; text-align:center">
+              Empowering domestic production Improve quality, quantity, and continuity through collaboration between stakeholders
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <p style="font-weight: 600; text-align:center">
+              Increase employment and inclusive economic growth
+            </p>
+          </div>
         </div>
       </div>
     </section>
